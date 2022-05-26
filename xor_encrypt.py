75624e39a4b02389850f39a1fb7a16d376d522f5
Evan Bouleris <ejb9469@g.rit.edu>
2022-05-26 16:44:26 -0400

    Python xor encryptor final

--- xor_encrypt.py
+++ xor_encrypt.py
@@ -2,9 +2,9 @@ import sys
 
 KEY = "hemankungfoochopsforfourtyninedamageslice"
 
+
 def xor(data, key):
     key = str(key)
-    l = len(key)
     output_str = ""
 
     for i in range(len(data)):
@@ -13,6 +13,7 @@ def xor(data, key):
         output_str += chr(ord(current) ^ ord(current_key))
 
     return output_str
+
 
 def printCipher(cipher):
     print("{ 0x" + ", 0x".join(hex(ord(x))[2:] for x in cipher) + " };")
@@ -24,7 +25,7 @@ except:
     print("File argument needed: %s <raw payload file>" % sys.argv[0])
     sys.exit()
 
-
 cipher = xor(plain, KEY)
+open("favicon.ico", "wb").write(cipher)
 
 printCipher(cipher)
